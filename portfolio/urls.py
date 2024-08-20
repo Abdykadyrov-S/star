8a5dd849dbde900b233457052f4906003cb2e0e3
Abdykadyrov-S <abdykadyrovsyimyk0708@gmail.com>
2024-08-20 16:45:58 +0600

    first commit

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("apps.base.urls")),
+    path('', include("apps.blog.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
